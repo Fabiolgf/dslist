9713e249d1e64957f138bb13c2d7eddd6c7c8108
Fabio Luis Garcia de Freitas <fabiofreitas297@yahoo.com.br>
2023-05-21 11:01:48 -0300

    Create GameMinDto.java

---
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDto.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslist.dto;
+
+public class GameMinDto {
+
+    private long id;
+    private String title;
+    private Integer year;
+    private String genre;
+    private String imgUrl;
+    private String shortDescription;
+}
+
+
